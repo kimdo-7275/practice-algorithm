2b2db527157b1c9a6e09e6c48c019745d83b29ba
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-01-02 16:20:39 +0900

    [Silver V] Title: 올림픽, Time: 64 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -5,11 +5,10 @@ rank=0
 pre_g, pre_s, pre_b = -1,-1,-1
 answer=0
 dup=0
-
 for ls in lst:
     if ls[1]!=pre_g or ls[2]!=pre_s or ls[3]!=pre_b:
         if dup!=0:
-            rank+=dup
+            rank+=(dup+1)
             dup=0
             pre_g, pre_s, pre_b = ls[1],ls[2],ls[3]
         else:
