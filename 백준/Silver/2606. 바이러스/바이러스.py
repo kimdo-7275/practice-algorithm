13759445e89586d6c9423e56e00ab18f67e14a0e
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-01-12 13:00:30 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,17 @@
-from collections import deque
 n=int(input())
 m=int(input())
-dp=[[0]*(n+1) for _ in range(n+1)]
-for i in range(m):
-    x,y=map(int,input().split())
-    dp[x][y]=1
-    dp[y][x]=1
-queue=deque()
-s=set()
-s.add(1)
-for i in range(n+1):
-    if dp[1][i]==1:
-        queue.append(i)
-while queue:
-    v=queue.popleft()
-    s.add(v)
+arr=[[0]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    i,j = map(int,input().split())
+    arr[i][j]=1
+    arr[j][i]=1
+visited=[0]*(n+1)
+q=[1]
+while q:
+    t=q.pop(0)
+    if visited[t]==0:
+        visited[t]=1
     for i in range(n+1):
-        if dp[v][i]==1 and i not in s:
-            s.add(i)
-            queue.append(i)
-print(len(s)-1)
+        if arr[t][i]==1 and visited[i]==0:
+            q.append(i)
+print(sum(visited)-1)
