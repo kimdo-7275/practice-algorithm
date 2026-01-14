4f7ebda5755cb97a2dddc4d9ed8c1110a9227c9a
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-01-14 18:22:43 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 380 ms, Memory: 41668 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,45 +1,29 @@
-import sys
 from collections import deque
-
-n, m = map(int, input().split())
-gp = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-visited = [[0 for _ in range(m)] for _ in range(n)]
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-
-# 2의 위치 찾기
-def find_goal(nn, mm, dp):
-    for i in range(nn):
-        for j in range(mm):
-            if dp[i][j] == 2:
-                return [i, j]
-
-
-# 2의 위치
-goal = find_goal(n, m, gp)
-goal_x = goal[0]
-goal_y = goal[1]
-
-q = deque()
-q.append((goal_x, goal_y))
-while q:
-    x, y = q.popleft()
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < n and 0 <= ny < m and gp[nx][ny] == 1 and visited[nx][ny] == 0:
-            q.append((nx, ny))
-            visited[nx][ny] = visited[x][y] + 1
-
-# 가지 못하는 1은 -1로 처리
+n,m=map(int,input().split())
+arr=[list(map(int,input().split())) for _ in range(n)]
+visit=[[0]*m for _ in range(n)]
+def bfs():
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j]!=2:
+                continue
+            q=deque()
+            q.append((i,j))
+            visit[i][j]=1
+            arr[i][j]=0
+            while q:
+                pi,pj=q.popleft()
+                for di,dj in ((-1,0),(1,0),(0,1),(0,-1)):
+                    ni,nj=pi+di,pj+dj
+                    if 0<=ni<n and 0<=nj<m and not visit[ni][nj] and arr[ni][nj]:
+                        visit[ni][nj]=1
+                        arr[ni][nj]=arr[pi][pj]+1
+                        q.append((ni,nj))
+            return
+bfs()
 for i in range(n):
     for j in range(m):
-        if visited[i][j] == 0 and gp[i][j] == 1:
-            visited[i][j] = -1
-# 답안 출력
-for i in range(n):
-    for j in range(m):
-        print(visited[i][j], end=' ')
-    print()
+        if not visit[i][j] and arr[i][j]:
+            arr[i][j]=-1
+for a in arr:
+    print(*a)
