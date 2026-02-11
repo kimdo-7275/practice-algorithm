dcc3089ed52d20849dd2861d83d3381f4c02f583
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-02-11 15:01:31 +0900

    [Gold IV] Title: 알파벳, Time: 888 ms, Memory: 51868 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,18 +1,24 @@
 import sys
-r,c=map(int,sys.stdin.readline().strip().split())
-arr=[list(sys.stdin.readline().strip()) for _ in range(r)]
-answer=0
-visit=set()
+r, c = map(int, sys.stdin.readline().split())
+board = [list(sys.stdin.readline().strip()) for _ in range(r)]
+def solve():
+    queue = set([(0, 0, board[0][0])])
+    max_dist = 0
 
-def dfs(pi,pj):
-    global answer
-    answer=max(answer,len(visit))
-    for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
-        ni,nj=pi+di,pj+dj
-        if 0<=ni<r and 0<=nj<c and arr[ni][nj] not in visit:
-            visit.add(arr[ni][nj])
-            dfs(ni,nj)
-            visit.remove(arr[ni][nj])
-visit.add(arr[0][0])
-dfs(0,0)
-print(answer)
+    while queue:
+        i, j, path = queue.pop()
+        max_dist = max(max_dist, len(path))
+
+        # 4방향 탐색
+        for di, dj in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            ni, nj = i + di, j + dj
+
+            if 0 <= ni < r and 0 <= nj < c:
+                # 새로운 알파벳인 경우만 큐에 추가
+                if board[ni][nj] not in path:
+                    queue.add((ni, nj, path + board[ni][nj]))
+
+    return max_dist
+
+
+print(solve())
