3006ca1e0757fd92e3fccd59877b4c226d892095
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-04-22 16:01:02 +0900

    [Silver III] Title: 블로그, Time: 164 ms, Memory: 58224 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,20 +1,28 @@
-n,x=map(int,input().split())
-lst=list(map(int,input().split()))
-
-visit=sum(lst[:x])
-mx=visit
-count=1 if visit else 0
-
-for i in range(x,n):
-    visit += (lst[i] - lst[i - x])
-    if visit==mx:
-        count+=1
-    elif visit>mx:
-        mx=visit
-        count=1
-
-if mx==0:
+N, X = map(int, input().split())
+visit_lst = list(map(int, input().split()))
+mx_visit = 0
+mx_day_cnt = 1
+s = 0
+e = s + X - 1
+for i in range(X):
+    mx_visit += visit_lst[i]
+tmp_visit = mx_visit - visit_lst[s]
+s += 1
+e += 1
+while e < N:
+    tmp_visit += visit_lst[e]
+    if mx_visit > tmp_visit:
+        pass
+    elif mx_visit < tmp_visit:
+        mx_visit = tmp_visit
+        mx_day_cnt = 1
+    elif mx_visit == tmp_visit:
+        mx_day_cnt+=1
+    tmp_visit-=visit_lst[s]
+    s+=1
+    e+=1
+if mx_visit==0:
     print('SAD')
 else:
-    print(mx)
-    print(count)
+    print(mx_visit)
+    print(mx_day_cnt)
