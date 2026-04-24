6734597be60cb576174edec3e362b349b7681734
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-04-24 23:07:06 +0900

    [Gold V] Title: 택배 배송, Time: 1524 ms, Memory: 54376 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,23 +1,21 @@
 import heapq
-n,m=map(int,input().split())
-arr=[[] for _ in range(n+1)]
-dist=[float('inf') for _ in range(n+1)]
-dist[1]=0
-for _ in range(m):
-    a,b,c=map(int,input().split())
-    arr[a].append([b,c])
-    arr[b].append([a,c])
-# arr에는 [노드, 비용]으로 저장
-hq=[[0,1]]  # 힙은 [비용,노드]로 저장 -> 비용이 제일 작은 거부터 보려고
-while hq:
-    cnt_dist,cnt_node = heapq.heappop(hq)
+N,M=map(int,input().split())
+roads=[[] for _ in range(N+1)]
+distance=[float('inf') for _ in range(N+1)]
 
-    if cnt_dist>dist[cnt_node]: # 이미 더 짧게 방문 넘어가주세요
+for _ in range(M):
+    a,b,d=map(int,input().split())
+    roads[a].append([b,d])
+    roads[b].append([a,d])
+distance[1]=0
+heap=[[0,1]]    # [거리,노드] 이렇게 저장하기
+while heap:
+    c_dis,c_nd=heapq.heappop(heap)
+    if distance[c_nd]<c_dis:
         continue
-
-    for nd,d in arr[cnt_node]:
-        dis=cnt_dist+d
-        if dist[nd]>dis:
-            dist[nd]=dis
-            heapq.heappush(hq,[dis,nd])
-print(dist[n])
+    for nd,dis in roads[c_nd]:
+        n_dis=c_dis+dis
+        if distance[nd]>n_dis:
+            distance[nd]=n_dis
+            heapq.heappush(heap,[n_dis,nd])
+print(distance[N])
