f2b732c7a618658165315d12ae8abcdd048df588
Dohyeong Kim <66244658+kimdo-7275@users.noreply.github.com>
2026-06-26 17:09:23 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,17 @@
+-- 코드를 작성해주세요
+SELECT 
+    ID,
+    CASE 
+        WHEN PCT <= 0.25 THEN 'CRITICAL'
+        WHEN PCT <= 0.50 THEN 'HIGH'
+        WHEN PCT <= 0.75 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END AS COLONY_NAME
+FROM (
+    SELECT 
+        ID,
+        PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS PCT
+    FROM ECOLI_DATA
+) AS SUB
+ORDER BY ID ASC;
+
